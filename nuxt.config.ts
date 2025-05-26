3b30bb9f3c7480d8fa3ba2ee70f92e6d5ca04cd6
webdev-dietrich <sascha_dietrich@outlook.com>
2025-05-26 10:56:06 +0200

    install and configure vuetify for nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint"],
+  build: {
+    transpile: ["vuetify"],
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook("vite:extendConfig", (config) => {
+        // @ts-expect-error "No Error occured"
+        config.plugins.push(vuetify({ autoImport: true }));
+      });
+    },
+    "@nuxt/eslint",
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
   eslint: {
     config: {
       standalone: false,
