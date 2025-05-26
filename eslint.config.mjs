cbf733fe56655141cdd992ece3a3c5e6c1353102
webdev-dietrich <sascha_dietrich@outlook.com>
2025-05-26 09:19:01 +0200

    linting

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import antfu from "@antfu/eslint-config";
+
+// @ts-check
+import withNuxt from "./.nuxt/eslint.config.mjs";
+
+export default withNuxt(
+  antfu({
+    type: "app",
+    vue: true,
+    typescript: true,
+    formatters: true,
+    stylistic: {
+      indent: 2,
+      semi: true,
+      quotes: "double",
+    },
+  }, {
+    rules: {
+      "ts/consistent-type-definitions": ["error", "type"],
+      "no-console": ["warn"],
+      "antfu/no-top-level-await": ["off"],
+      "node/prefer-globals/process": ["off"],
+      "node/no-process-env": ["error"],
+      "perfectionist/sort-imports": ["error", {
+        tsconfigRootDir: ".",
+      }],
+      "unicorn/filename-case": ["error", {
+        case: "camelCase",
+        ignore: ["README.md"],
+      }],
+    },
+  }),
+);
