9fd5ca75c06275da1f53d50c899084ec0779a95b
webdev-dietrich <sascha_dietrich@outlook.com>
2025-05-28 14:55:04 +0200

    add analytics from vercel configure analytics

--- app.vue
+++ app.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
+import { Analytics } from "@vercel/analytics/nuxt";
+import { SpeedInsights } from "@vercel/speed-insights/vue";
 </script>
 
 <template>
+  <Analytics />
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
