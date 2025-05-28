3a1bf4e66d0d85dd2b5a649fadc050dcf7c3e748
webdev-dietrich <sascha_dietrich@outlook.com>
2025-05-28 14:27:56 +0200

    add speedinsigths from vercel

--- app.vue
+++ app.vue
@@ -1,4 +1,9 @@
+<script setup lang="ts">
+import { SpeedInsights } from "@vercel/speed-insights/dist/vue/index";
+</script>
+
 <template>
+  <SpeedInsights />
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
---
+++ pages/privacy.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <v-container>
+    <h1>Datenschutz</h1>
+    <VIcon icon="mdi-plus" />
+  </v-container>
+</template>
+
+<style scoped>
+
+</style>
