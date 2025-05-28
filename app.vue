aadc51ef89dcfafd993d3f63a7f2eed4407b2ecb
webdev-dietrich <sascha_dietrich@outlook.com>
2025-05-28 14:46:27 +0200

    add speedinsigths from vercel reconfigure part 2

--- app.vue
+++ app.vue
@@ -1,17 +1,9 @@
 <script setup lang="ts">
-useHead({
-  script: [
-    {
-      "src": "https://speed.vercel.dev/script.js",
-      "data-sid": "prj_QZgPeH1HakOzLAHRYEVU5Gs5b51h",
-      "defer": true,
-    },
-  ],
-});
 </script>
 
 <template>
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
+  <SpeedInsights />
 </template>
